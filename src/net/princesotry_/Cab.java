d2eec4a48c83ecd40bab4e81e78d520616bdf525
DeadEndOfTheRoad <108024605+DeadEndOfTheRoad@users.noreply.github.com>
2022-07-24 19:10:19 +0100

    New Project

--- src/net/princesotry_/Cab.java
+++ src/net/princesotry_/Cab.java
@@ -1,5 +1,5 @@
 package net.princesotry_;
 
 public interface Cab {
-
+    void bookCab();
 }
--- src/net/princesotry_/Main.java
+++ src/net/princesotry_/Main.java
@@ -1,15 +1,8 @@
 package net.princesotry_;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.function.Supplier;
-
 public class Main{
 
-    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     public static void main(String[] args) {
-
 
     }
 
